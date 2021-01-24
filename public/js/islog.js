514738b7a8fbcede8e1728722370bc146fa30d7c
Olga Illarionova <ola.illari@gmail.com>
2021-01-23 17:47:11 -0800

    print user name on profile page if user is logged

--- public/js/islog.js
+++ public/js/islog.js
@@ -1,39 +1,14 @@
 
 $(document).ready(function() {
-    /**
-     * check if users?
-     * $.get("/api/user_data").then(function(data){
-     * if (!data){
-     * ///show login/signup
-     * $(#log-div).show();
-     * $(#acc-div).hide();
-     * }
-     * else{
-     * ///show link to account page
-     * $(#log-div).hide();
-     * $(#acc-div).show();
-     * }
-     * })
-     */
+   console.log("privet");
     $.get("/api/user_data").then(function(data){
-        if(JSON.stringify(data)==='{}'){
-            /// show login sign up
-            $("#login").show();
-            $("#signup").show();
-            // hide manage acc, logout
-            $("#logout").hide();
-            $("#manage").hide()
+        console.log(data);
+        if(JSON.stringify(data)!=='{}'){
+            /// show name
+            let fullName = data.firstName + ' '+data.lastName;
+            let el = $("#firstLast")
+            el.text(fullName);
         }
-        else{
-            ///hide login signup 
-            $("#login").hide();
-            $("#signup").hide();
-            // show logout 
-            $("#logout").show();
-            $("#manage").show()
-            // and name
-            //let fullName = data.firstName + ' '+data.lastName;
-            //console.log(fullName);
-        }
+        
     })
 });
